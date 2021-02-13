c89f592eca8d474b5d80d55e1b2b460372f24c35
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-12 16:03:16 -0800

    the update and edit form are created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
